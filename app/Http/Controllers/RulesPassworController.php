60d4c388e868e0b3b3ca98f27a548c6a8183364a
Leonardo Lima <llimaesilva7@gmail.com>
2023-01-25 17:42:19 -0300

    método para avaliar o tamanho mínimo da senha

--- app/Http/Controllers/RulesPassworController.php
+++ app/Http/Controllers/RulesPassworController.php
@@ -6,7 +6,30 @@ use Illuminate\Http\Request;
 
 class RulesPassworController extends Controller
 {
-    public function verify(Request $request) {
-        
+    public $request;
+    public function __construct(Request $request) {
+        $this->request = $request;
+    }
+    public function verify(Request $request) 
+    {
+        // dd($request->rules);
+        $this->rulesArray($this->request->rules);
+        // return response()->json([
+        //     "password" => $request->password, 
+        //     "rules" => $request->rules
+        // ]);
+    }
+   
+    public function rulesArray($rules)
+    {
+        foreach ($rules as $key => $rule) {
+            // dd($rule);
+           dd(call_user_func_array(array($this, $rule['rule']), array($rule['rule'], $rule['value'])));
+        }
+    }
+
+    protected function minSize(String $name, Int $value) {
+        $expression = "/[\w_@.\/!#&+-]{{$value},}/i";
+        return ["rule" => $name, "verify" => preg_match($expression, $this->request->password)];
     }
 }
