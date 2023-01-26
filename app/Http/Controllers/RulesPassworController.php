ce48289df39e046e8f43fc43a80fa61d1777329e
Leonardo Lima <llimaesilva7@gmail.com>
2023-01-26 15:23:34 -0300

    removendo metodo de verificar regras para um serviço

--- app/Http/Controllers/RulesPassworController.php
+++ app/Http/Controllers/RulesPassworController.php
@@ -25,20 +25,6 @@ class RulesPassworController extends Controller
         // ]);
     }
    
-    public function rulesArray($rules)
-    {
-        $validationResult = [];
-        try {
-            foreach ($rules as $key => $rule) {
-                call_user_func_array(array($this, $rule['rule']), array($rule['rule'], $rule['value']))['verify'] ? '' : array_push($validationResult, call_user_func_array(array($this, $rule['rule']), array($rule['rule'], $rule['value']))['rule']);
-            }
-            
-            return sizeof($validationResult) ? ["verify" => false, "noMatch" => $validationResult] : ["verify" => true, "noMatch" => $validationResult];
-        } catch (\Exception $e) {
-            return ["error" => $e->getMessage()];
-        }
-    }
-
     protected function minSize(String $name, Int $value) {
         $expression = "/[_@.\/!#$%^&*\(\)+-\{\}\[\]]{{$value},}/i";
         return ["rule" => $name, "verify" => preg_match($expression, $this->request->password)];
