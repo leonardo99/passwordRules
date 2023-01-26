2afeebab8b7413853c679b4faf67f84ce29a624f
Leonardo Lima <llimaesilva7@gmail.com>
2023-01-26 17:54:28 -0300

    inserindo tipagem aos retornos dos metodos

--- app/Services/RuleService.php
+++ app/Services/RuleService.php
@@ -11,7 +11,8 @@ class RuleService
     public function __construct(Request $request) {
         $this->request = $request;
     }
-    public function verifyRules($rules)
+
+    public function verifyRules(Array $rules): Array
     {
         $validationResult = [];
         try {
@@ -25,36 +26,36 @@ class RuleService
         }
     }
 
-    protected function minSize(String $name, Int $value) {
+    protected function minSize(String $name, Int $value): Array {
         $expression = "/[_@.\/!#$%^&*\(\)+-\{\}\[\]]{{$value},}/i";
         return ["rule" => $name, "verify" => preg_match($expression, $this->request->password)];
     }
 
-    protected function minSpecialChars(String $name, Int $value) {
+    protected function minSpecialChars(String $name, Int $value): Array {
         //(?:.*[-+_!@#$%^&*., ?]) representa pelo menos um caractere especial.
         $expression = "/(?:.*[_@.\/!#$%^&*\(\)+-\{\}\[\]]){{$value},}/i";
         return ["rule" => $name, "verify" => preg_match($expression, $this->request->password)];
     }
 
-    protected function minUppercase(String $name, Int $value) {
+    protected function minUppercase(String $name, Int $value): Array {
         //(?:.*[A-Z]) representa pelo menos um caractere maiúsculo.
         $expression = "/(?:.*[A-Z]){{$value},}/";
         return ["rule" => $name, "verify" => preg_match($expression, $this->request->password)];
     }
 
-    protected function minLowercase(String $name, Int $value) {
+    protected function minLowercase(String $name, Int $value): Array {
         //(?:.*[a-z]) representa pelo menos um caractere minúsculo.
         $expression = "/(?:.*[a-z]){{$value},}/";
         return ["rule" => $name, "verify" => preg_match($expression, $this->request->password)];
     }
 
-    protected function minDigit(String $name, Int $value) {
+    protected function minDigit(String $name, Int $value): Array {
         //(?:.*[a-z]) representa pelo menos um caractere minúsculo.
         $expression = "/(?:.*[0-9]){{$value},}/";
         return ["rule" => $name, "verify" => preg_match($expression, $this->request->password)];
     }
     
-    protected function noRepeted(String $name) {
+    protected function noRepeted(String $name): Array {
         $expression = "/(.)\\1+/i";
         return ["rule" => $name, "verify" => preg_match($expression, $this->request->password) ? 0 : 1];
     }
