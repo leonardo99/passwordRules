a0f55977e361f7a2022922347581407015bcc145
Leonardo Lima <llimaesilva7@gmail.com>
2023-01-27 19:12:48 -0300

    otimizando expressão regular

--- app/Repository/RuleRepository.php
+++ app/Repository/RuleRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 class RuleRepository 
 {
     protected function minSize(String $name, Int $value): Array {
-        $expression = "//";
+        $expression = "/[_@.\/!#$%^&*\(\)+-\{\}\[\]]{{$value},}/i";
         return ["rule" => $name, "verify" => preg_match($expression, $this->request->password)];
     }
 
